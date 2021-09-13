7566e4bc2bb8f25113962ee4dfe49765f60bc7ee
nnabuezeuhiara <nuhiara@gmail.com>
2021-09-13 20:06:51 +0100

    Initial Commit

---
+++ WeatherTomorrow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherTomorrow
+//
+//  Created by Nnabueze Uhiara on 13/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherTomorrow/WeatherTomorrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTomorrowApp.swift
+//  WeatherTomorrow
+//
+//  Created by Nnabueze Uhiara on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTomorrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
